d261ff931c09e735c9cf3213e6ffda0396019164
Alessandro Mecca <alessandro.mecca@gmail.com>
2010-04-30 02:54:24 +0200

    Added copyright notice in every java file

--- src/main/java/com/neoprojectmanager/model/NodeWrapper.java
+++ src/main/java/com/neoprojectmanager/model/NodeWrapper.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2010. Alessandro Mecca <alessandro.mecca@gmail.com>
+ * All rights reserved.
+ *
+ * See LICENSE file for details.
+ */
+
 package com.neoprojectmanager.model;
 
 import org.apache.commons.lang.NotImplementedException;
--- src/main/java/com/neoprojectmanager/utils/XmlUtils.java
+++ src/main/java/com/neoprojectmanager/utils/XmlUtils.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2010. Alessandro Mecca <alessandro.mecca@gmail.com>
+ * All rights reserved.
+ *
+ * See LICENSE file for details.
+ */
+
 package com.neoprojectmanager.utils;
 
 public class XmlUtils {
